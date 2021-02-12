26d750878ceb98099561335cced794edc0ee924c
Jeff Villarina <villarina.jeffrey@gmail.com>
2021-02-12 13:46:25 -0800

    nextjs blog

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Jeff V'
+const name = 'Coder'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
